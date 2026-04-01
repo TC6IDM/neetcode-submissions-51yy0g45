b80aa35aaf656b590f0d70b4a0b151c85dac2125
Andrew Tissi <67088794+TC6IDM@users.noreply.github.com>
2026-04-01 13:23:30 -0400

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        dir_ = []
+        for i in logs:
+
+            if i[0]!= ".":
+                dir_.append(i[:-1])
+
+            if i[0] == "." and i[1] == ".":
+                if dir_ != []:
+                    dir_ = dir_[:-1]
+            
+            if i[0] == "." and i[1] == "/":
+                pass
+        print(dir_)
+
+        return len(dir_)
